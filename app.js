1dcd7f05dea5463ddaad8d5c0f9e0f7b25d0969f
justgast <justgast@gmail.com>
2022-12-26 19:12:32 +0300

    Part 4: Exercise 4.15 ready;

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
 const blogsRouter = require('./controllers/blog');
+const usersRouter = require('./controllers/user');
 const middleware = require('./utils/middleware');
 
 const app = express();
@@ -23,6 +24,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- models/user.js
+++ models/user.js
@@ -1,34 +1,31 @@
 const mongoose = require('mongoose');
 
-const blogSchema = new mongoose.Schema({
-  title: {
+const userSchema = new mongoose.Schema({
+  username: {
     type: String,
     required: true,
   },
-  author: {
+  name: {
     type: String,
     required: true,
   },
-  url: {
+  passwordHash: {
     type: String,
     required: true,
-  },
-  likes: {
-    type: Number,
-    default: 0,
   },
 });
 
-blogSchema.set('toJSON', {
+userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     /* eslint-disable */
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
+    delete returnedObject.passwordHash;
     /* eslint-enable */
   },
 });
 
-const Blog = mongoose.model('Blog', blogSchema);
+const User = mongoose.model('User', userSchema);
 
-module.exports = Blog;
+module.exports = User;
